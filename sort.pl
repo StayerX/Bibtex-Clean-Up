8d458ecbb8c6ad09859870344a36b99eb4e3d3c8
Stanislav Yotov <yotov.st@gmail.com>
2015-11-01 15:29:44 -0600

    Perl Script for cleaning up big bibtrx files.

---
+++ sort.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/env perl
+# sudo apt-get install libtext-bibtex-perl
+
+use strict;
+use warnings;
+use Text::BibTeX;
+
+my $input_filename = "thesis.bib";
+system(qq|perl -i -pe 's/month = (\\w+),/month = \\{\$1\\},/' $input_filename|);
+
+
+my $bibfile = Text::BibTeX::File->new($input_filename);
+my $newfile = Text::BibTeX::File->new(">newthesis.bib");
+
+my @entries;
+ 
+my %keys;
+my $count = 0;
+while (my $entry = Text::BibTeX::Entry->new( $bibfile ) ) {
+print $entry;
+	#next unless $entry->parse_ok;
+	die $entry->key unless $entry->parse_ok;
+	my $key = $entry->key;
+	warn "Duplicate key $key" if exists $keys{ $key };
+	$keys{ $key } = 1;
+
+	#my $entry_text = $entry->print_s;
+	#$entry_text =~ s/\n//gms;
+	#print "$entry_text\n";
+	$count = $count+1;
+	push @entries, $entry;
+}
+
+print $count;
+my @sorted = sort {
+	my ($a_key, $a_year, $a_title, $a_author) = ($a->key, $a->get('year', 'title', 'author'));
+	my ($b_key, $b_year, $b_title, $b_author) = ($b->key, $b->get('year', 'title', 'author'));
+	my ( $k,  $y, $t, $a ) = (
+		defined $a_key && defined $b_key ? $a_key cmp $b_key : 0,
+		defined $a_year && defined $b_year ? $a_year <=> $b_year : 0,
+		defined $a_title && defined $b_title ? $a_title cmp $b_title : 0,
+		defined $a_author && defined $b_author ? $a_author cmp $b_author : 0,
+	);
+	$k or $y or $t or $a;
+} @entries;
+
+for my $entry ( @sorted ) {
+	$entry->write ($newfile);
+}
+
