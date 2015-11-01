d13ea1fd1927d3c4d8720b9050fa296141214331
StayerX <yotov.st@gmail.com>
2015-11-01 16:05:14 -0600

    Update sort.pl

--- sort.pl
+++ sort.pl
@@ -5,15 +5,19 @@ use strict;
 use warnings;
 use Text::BibTeX;
 
+# File for parsing and cleaning up
+# You may change it to my $input_filename = $ARGV[1];
 my $input_filename = "thesis.bib";
+
+# Temporary fix for bug https://rt.cpan.org/Public/Bug/Display.html?id=98806
 system(qq|perl -i -pe 's/month = (\\w+),/month = \\{\$1\\},/' $input_filename|);
 
 
 my $bibfile = Text::BibTeX::File->new($input_filename);
-my $newfile = Text::BibTeX::File->new(">newthesis.bib");
+my $newfile = Text::BibTeX::File->new(">newthesis.bib"); # Output file
 
 my @entries;
- 
+
 my %keys;
 my $count = 0;
 while (my $entry = Text::BibTeX::Entry->new( $bibfile ) ) {
