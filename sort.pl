ddaf0b921b56d87b1929ad7dd490563477298f26
Stanislav Yotov <yotov.st@gmail.com>
2015-11-01 18:34:21 -0600

    Relative file path setup

--- sort.pl
+++ sort.pl
@@ -4,17 +4,23 @@
 use strict;
 use warnings;
 use Text::BibTeX;
+use File::Basename;
+
+
+
+my $dirname = dirname(__FILE__);
 
 # File for parsing and cleaning up
 # You may change it to my $input_filename = $ARGV[1];
-my $input_filename = "thesis.bib";
+print $dirname;
+my $input_filename = "${dirname}/thesis.bib";
 
 # Temporary fix for bug https://rt.cpan.org/Public/Bug/Display.html?id=98806
 system(qq|perl -i -pe 's/month = (\\w+),/month = \\{\$1\\},/' $input_filename|);
 
 
 my $bibfile = Text::BibTeX::File->new($input_filename);
-my $newfile = Text::BibTeX::File->new(">newthesis.bib"); # Output file
+my $newfile = Text::BibTeX::File->new(">${dirname}/newthesis.bib"); # Output file
 
 my @entries;
 
